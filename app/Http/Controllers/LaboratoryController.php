49982c013071df0a9d529ef547732120f928bc77
DjoleK97 <djolefon@gmail.com>
2022-09-21 17:47:42 +0200

    dodati kontroleri i putanje ka serveru

---
+++ app/Http/Controllers/LaboratoryController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Laboratory;
+use Illuminate\Http\Request;
+
+class LaboratoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json(Laboratory::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $laboratory = Laboratory::create($request->all());
+        return response()->json($laboratory);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Laboratory  $laboratory
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Laboratory $laboratory)
+    {
+        return response()->json([
+            "id" => $laboratory->id,
+            "name" => $laboratory->name,
+            "created_at" => $laboratory->created_at,
+            "updated_at" => $laboratory->updated_at,
+            "address" => $laboratory->address,
+            "phone" => $laboratory->phone,
+            "email" => $laboratory->email,
+            "analyses" => $laboratory->analyses,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Laboratory  $laboratory
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Laboratory $laboratory)
+    {
+        $laboratory->update($request->all());
+        return response()->json($laboratory);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Laboratory  $laboratory
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Laboratory $laboratory)
+    {
+        $laboratory->delete();
+        return response()->noContent();
+    }
+}
